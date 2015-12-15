c51196e42db6ca98bd2356c6f3badf680dd1981b
Susan <senelson@mindspring.com>
2015-12-14 17:02:04 -0800

    initial project creation

---
+++ src/main/java/com/susannelson/PageParserApplication.java
@@ -0,0 +1,12 @@
+package com.susannelson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PageParserApplication.class, args);
+    }
+}
---
+++ src/test/java/com/susannelson/PageParserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.susannelson;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PageParserApplication.class)
+public class PageParserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
